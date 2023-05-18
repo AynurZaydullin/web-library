55f32b64cf0f3400b370db9a69c92cffe2aff713
AynurZaydullin <zaydullin.aynur@yandex.ru>
2023-05-18 16:40:09 +0300

    Начало выполнения учебного задания по теме Spring Web.

--- src/main/java/ru/skypro/lessons/springboot/weblibrary/controller/EmployeeController.java
+++ src/main/java/ru/skypro/lessons/springboot/weblibrary/controller/EmployeeController.java
@@ -1,6 +1,8 @@
 package ru.skypro.lessons.springboot.weblibrary.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.skypro.lessons.springboot.weblibrary.service.EmployeeService;
@@ -12,13 +14,10 @@ import java.util.OptionalInt;
 
 @RestController
 @RequestMapping("/employee")
+@RequiredArgsConstructor
 public class EmployeeController {
 
     private final EmployeeService employeeService;
-
-    public EmployeeController(EmployeeService employeeService) {
-        this.employeeService = employeeService;
-    }
 
     @GetMapping("/employees")
     public List<Employee> showAllEmployees() {
@@ -40,5 +39,11 @@ public class EmployeeController {
     @GetMapping("/salary/high-salary")
     public List<Employee> showHighSalariesOfEmployees() {
         return employeeService.getHighSalariesOfEmployees();
+    }
+    // Создаем эндпоинт для подсчета сотрудников в указанном отделе
+    @GetMapping("/count/{division}")
+    public int getDivisionEmployeesCount(@PathVariable String division) {
+        // ...
+        return 0;
     }
 }
